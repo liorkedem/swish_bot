caad32915e0fd8ca4416e828f3ae0c9062428bdd
liorkedem <liorke@monday.com>
2021-11-19 18:54:37 +0200

    spaces

--- constants/stats-constants.js
+++ constants/stats-constants.js
@@ -1,4 +1,5 @@
 const PERCENTAGE_CATEGORIES = ["FG", "3PT", "FT"];
+
 const SIMPLE_CATEGORIES = [
   "MIN",
   "OREB",
@@ -21,6 +22,7 @@ const DFS_RATIO = {
   REB: 1.2,
   TO: -1,
 };
+
 const ROTO_8CAT_RATIO = {
   PTS: 1.333, //1
   AST: 4.579,
@@ -31,15 +33,18 @@ const ROTO_8CAT_RATIO = {
   FGPFIXED: 2.724,
   FTPFIXED: 5.762,
 };
+
 const ROTO_9CAT_RATIO = {
   ...ROTO_8CAT_RATIO,
   TO: -8.024,
 };
+
 const PCT_BOUNDS = {
   "3PTP": { low: 0.364, high: 0.48 },
   FGP: { low: 0.458, high: 0.59 },
   FTP: { low: 0.782, high: 1.0 },
 };
+
 module.exports = {
   PERCENTAGE_CATEGORIES,
   SIMPLE_CATEGORIES,
