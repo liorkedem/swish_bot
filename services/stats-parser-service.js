920754a13e5cf64a6d0cb8ab787c08185cd435f6
liorkedem <liorke@monday.com>
2021-11-19 17:32:18 +0200

    fix

--- services/stats-parser-service.js
+++ services/stats-parser-service.js
@@ -26,19 +26,19 @@ class StatsParserService {
 
     boxScore["2PTA"] = _.subtract(boxScore.FGA, boxScore["3PTA"]);
     boxScore["2PTM"] = _.subtract(boxScore.FGM, boxScore["3PTM"]);
-    boxScore["2PTP"] = _.divide(boxScore["2PTM"], boxScore["2PTA"]);
+    boxScore["2PTP"] = _.divide(boxScore["2PTM"], boxScore["2PTA"]) || 0;
 
     return boxScore;
   }
 
   static parseCategory(category, value) {
     if (SIMPLE_CATEGORIES.includes(category)) {
-      return { [category]: _.parseInt(value) };
+      return { [category]: _.parseInt(value) || 0 };
     } else if (PERCENTAGE_CATEGORIES.includes(category)) {
       const values = _.split(value, "-");
       if (!_.isEmpty(values)) {
-        const madeValue = _.parseInt(values[0]);
-        const attemptsValue = values[1];
+        const madeValue = _.parseInt(values[0]) || 0;
+        const attemptsValue = values[1] || 0;
         const madeCategory = `${category}M`;
         const attemptsCategory = `${category}A`;
         const pctCategory = `${category}P`;
