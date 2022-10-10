cea9926b6b24d41471eafc2041c2bbd5f1506657
Lior Kedem <liorke@monday.com>
2022-10-10 20:16:54 +0300

    step 1

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ const express = require("express");
 const router = express.Router();
 const ScheduleController = require("../controllers/schedule-controller");
 const GameController = require("../controllers/game-controller");
+const TeamController = require("../controllers/team-controller");
 const PlayerController = require("../controllers/player-controller");
-const BasketballReferenceController = require("../controllers/basketball-reference-controller");
+// const BasketballReferenceController = require("../controllers/basketball-reference-controller");
 const SwishController = require("../controllers/swish-controller");
 
 /* GET home page. */
@@ -19,9 +20,10 @@ router.get("/health", function (req, res, next) {
 router.get("/get-ended-games", ScheduleController.getEndedGames);
 router.get("/get-non-ended-games", ScheduleController.getNonEndedGames);
 router.get("/get-game-top-players", GameController.getGameTopPlayers);
+router.get("/get-team-players", TeamController.getTeamPlayers);
 router.get("/get-player", PlayerController.getPlayer);
-router.get("/get-team-players", BasketballReferenceController.getTeamPlayers);
-router.get("/get-daily-leaders", BasketballReferenceController.getDailyLeaders);
+// router.get("/get-team-players", BasketballReferenceController.getTeamPlayers);
+// router.get("/get-daily-leaders", BasketballReferenceController.getDailyLeaders);
 router.get(
   "/add-update-players-basic-info",
   SwishController.addUpdatePlayersBasicInfo
--- services/player-service.js
+++ services/player-service.js
@@ -13,6 +13,39 @@ class PlayerService {
 
     return { ...player, info: playerInfo, profile: playerProfile };
   }
+
+  static serializePlayer(player, team) {
+    const { id, fullName: name, displayHeight, position } = player;
+
+    const serializedPlayer = {
+      id,
+      name,
+      position: serializePosition(position.abbreviation),
+      height: serializeHeight(displayHeight),
+      team: team.abbreviation,
+      health: "",
+      image: "",
+      image_credit: "",
+      image_credit_url: "",
+      extra_info: "",
+    };
+
+    return serializedPlayer;
+  }
+}
+
+function serializePosition(position) {
+  switch (position) {
+    case "G":
+      return "PG/SG";
+    case "F":
+      return "SF/PF";
+  }
+  return position;
+}
+
+function serializeHeight(height) {
+  return height.replace("'", "").replace('"', "").replace(" ", "-");
 }
 
 module.exports = PlayerService;
