a6dec4346f9865fa0ad9c89850a4b2aaadb015ba
Lior Kedem <liorke@monday.com>
2022-10-10 22:24:11 +0300

    more

--- controllers/swish-controller.js
+++ controllers/swish-controller.js
@@ -1,41 +1,6 @@
 const _ = require("lodash");
-const BasketballReferenceService = require("../services/basketball-reference-service");
 const TeamService = require("../services/team-service");
 const SwishService = require("../services/swish-service");
-const { format } = require("morgan");
-const { forEach } = require("lodash");
-const TEAM_IDS = [
-  "ATL",
-  "BOS",
-  "BRK",
-  "CHO",
-  "CHI",
-  "CLE",
-  "DAL",
-  "DEN",
-  "DET",
-  "GSW",
-  "HOU",
-  "IND",
-  "LAC",
-  "LAL",
-  "MEM",
-  "MIA",
-  "MIL",
-  "MIN",
-  "NOP",
-  "NYK",
-  "OKC",
-  "ORL",
-  "PHI",
-  "PHO",
-  "POR",
-  "SAC",
-  "SAS",
-  "TOR",
-  "UTA",
-  "WAS",
-];
 
 class SwishController {
   static async addUpdatePlayersBasicInfo(req, res) {
